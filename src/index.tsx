f420e4c5c3c3f5a3daeb4b5749d5753629c894d8
jensen yuen <jensenyuen@outlook.com>
2023-02-02 21:08:02 +0900

    footerファイル追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './stylesheets/index.css'
+import './stylesheets/index.scss'
 import './i18n'
 
 const root = ReactDOM.createRoot(
