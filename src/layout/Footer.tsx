fb85ee551e8aa0984bbb27f7763098f4cacf2fdf
jensen yuen <jensenyuen@outlook.com>
2023-02-23 18:24:48 +0900

    footer編集

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
+
   return (
-    <>
-    </>
+    <div className="footer">
+      {`© ${year} Jensen Yuen. All Rights Reserved.`}
+    </div>
   )
 }
 
