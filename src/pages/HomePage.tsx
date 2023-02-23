e588fe929d1c27655e05f31ce2b5bdab043f205d
jensen yuen <jensenyuen@outlook.com>
2023-02-24 00:15:55 +0900

    コンテンツ編集

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,9 +14,13 @@ const HomePage = () => {
       <Navbar />
       <Container maxWidth='sm'>
         <div className="d-flex flex-column align-center text-center mb-5">
-          <img src={profile} alt='profile picture' className='profile-image mb-2'/>
-          <span>
-            Hello! My name is Jensen. I am a fullstack developer.
+          <img src={profile} alt='profile picture' className='profile-image mb-3'/>
+          <div className='d-flex align-center'>
+            <h2 className='mt-0 mb-2 mr-1'>Jensen Yuen</h2>
+            (Software Engineer)
+          </div>
+          <span className='quote'>
+            Hello, I aspire to be a fullstack web developer!
           </span>
         </div>
         <div className='mb-5'>
@@ -29,6 +33,10 @@ const HomePage = () => {
             <span className='mr-2'>2021</span>
             Graduated from Nanyang Technological University, Bachelor of Engineering
             Specialization: Electronic Engineering (Microelectronics & Photonics)
+          </div>
+          <div>
+            <span className='mr-2'>2021</span>
+            Completed Web Development Course from LeWagon
           </div>
         </div>
       </Container>
