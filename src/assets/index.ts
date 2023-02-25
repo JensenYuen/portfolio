dfdc0f14044c6b80b59c1ecec3e8c0895ada2ab4
jensen yuen <jensenyuen@outlook.com>
2023-02-25 19:03:49 +0900

    aboutpage編集

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,5 +4,7 @@ import scubadoo from './scubadoo.png'
 import profile from './profile.png'
 import prevPort from './prevPort.png'
 import popuGraph from './popuGraph.png'
+import reactLogo from './reactLogo.png'
+import materialUi from './materialUi.png'
 
-export { gathersg, offgrid, scubadoo, profile, prevPort, popuGraph }
+export { gathersg, offgrid, scubadoo, profile, prevPort, popuGraph, reactLogo, materialUi }
