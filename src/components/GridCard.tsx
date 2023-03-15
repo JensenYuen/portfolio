c1511b475cecaa561c51e13981dbd2a4ae124d6d
jensen yuen <jensenyuen@outlook.com>
2023-03-15 23:40:17 +0900

    minor

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -11,7 +11,7 @@ interface props {
 const GridCard = ({ className, title, imgSrc, altText, description }: props) => {
   return (
     <div>
-      <img src={imgSrc} alt={altText} className={className}/>
+      <img src={imgSrc} alt={altText} className={className} loading='lazy'/>
       <h3 className='mt-1 mb-0 text-center'>{title}</h3>
       <p className='mt-0 mb-0 text-center'>{description}</p>
     </div>
