377a96da069989719d949bdbe54cc04bea9858e6
Jensen <jensenyuen.e@gmail.com>
2024-04-14 20:35:01 +0900

    Moved images to online service

--- src/assets/index.ts
+++
@@ -1,9 +0,0 @@
-import caseconnect from './caseConnect.png'
-import offgrid from './offgrid.png'
-import scubadoo from './scubadoo.png'
-import profile from './profile.png'
-import popuGraph from './popuGraph.png'
-import reactLogo from './reactLogo.png'
-import materialUi from './materialUi.png'
-
-export { caseconnect, offgrid, scubadoo, profile, popuGraph, reactLogo, materialUi }
