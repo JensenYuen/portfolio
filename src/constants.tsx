c01ace81c0d7e3e1aa89e81ae6410bb5285ccbee
Jensen <jensenyuen.e@gmail.com>
2024-04-18 00:34:20 +0900

    added constant data

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,30 +1,37 @@
 // Image URL
-export const PROFILE_IMG = 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037235/portfolio_use/profile_ls5wa1.png'
-export const OFFGRID_IMG = 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037158/portfolio_use/offgrid_tk01oz.png'
-export const SCUBADOO_IMG = 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037153/portfolio_use/scubadoo_bzuafx.png'
-export const POPUGRAPH_IMG = 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037148/portfolio_use/popuGraph_m9qcd1.png'
-export const CASECONNECT_IMG = 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037130/portfolio_use/caseConnect_u6mqi4.png'
-export const REACT_IMG = 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037143/portfolio_use/reactLogo_lhz1mi.png'
-export const MATERIALUI_IMG = 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037134/portfolio_use/materialUi_hr4yyb.png'
+export const IMGURL = {
+  profile: 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037235/portfolio_use/profile_ls5wa1.png',
+  offgrid: 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037158/portfolio_use/offgrid_tk01oz.png',
+  scubadoo: 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037153/portfolio_use/scubadoo_bzuafx.png',
+  popugraph: 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037148/portfolio_use/popuGraph_m9qcd1.png',
+  caseconnet: 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037130/portfolio_use/caseConnect_u6mqi4.png',
+  react: 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037143/portfolio_use/reactLogo_lhz1mi.png',
+  materialui: 'https://res.cloudinary.com/dwxeen3eo/image/upload/v1713037134/portfolio_use/materialUi_hr4yyb.png'
+}
 
 // Image Data
-export const IMGDATA = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
+export const IMGDATA = {
+  offgrid: [{
+    img: IMGURL.offgrid,
+    title: 'Main page',
     featured: true
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    featured: false
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    featured: false
-  }
-]
+  }],
+  scubadoo: [{
+    img: IMGURL.scubadoo,
+    title: 'Main page',
+    featured: true
+  }],
+  popugraph: [{
+    img: IMGURL.popugraph,
+    title: 'App page',
+    featured: true
+  }],
+  caseconnect: [{
+    img: IMGURL.caseconnet,
+    title: 'Caseconnet',
+    featured: true
+  }]
+}
 
 // Works Info
 export const WORKSDATA = [
@@ -33,27 +40,27 @@ export const WORKSDATA = [
     subText: 'work.popugraph.subtext',
     altText: 'Image of popu-graph site',
     link: 'popugraph',
-    imgSrc: POPUGRAPH_IMG
+    imgSrc: IMGURL.popugraph
   },
   {
     title: 'work.caseconnect.title',
     altText: 'Image of caseconnect site',
     subText: 'work.caseconnect.subtext',
     link: 'caseconnect',
-    imgSrc: CASECONNECT_IMG
+    imgSrc: IMGURL.caseconnet
   },
   {
     title: 'work.offgrid.title',
     subText: 'work.offgrid.subtext',
     altText: 'Image of offgrid site',
     link: 'offgrid',
-    imgSrc: OFFGRID_IMG
+    imgSrc: IMGURL.offgrid
   },
   {
     title: 'work.scubadoo.title',
     subText: 'work.scubadoo.subtext',
     altText: 'Image of scubadoo site',
     link: 'scubadoo',
-    imgSrc: SCUBADOO_IMG
+    imgSrc: IMGURL.scubadoo
   }
 ]
